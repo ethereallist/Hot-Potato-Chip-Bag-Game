61b91511921a5e84c548d5fcf8c45b863ba9f483
Au-Sc <210092108+Au-Sc@users.noreply.github.com>
2026-09-18 21:25:41 -0400

    Add PlayerTracker class
    Add PlayerTracker class for utilities related to player controllers and input

--- settings.py
+++ settings.py
@@ -38,37 +38,37 @@ if _gamecontrollerdb_path.exists():
 pygame.init()
 InputHandler.init_gamepads()
 
-# --- Jugador 1: teclado (WASD + espacio dash + E secundaria) ---
+# --- Jugador 1: teclado (WASD + E dash + Q secundaria) ---
 InputHandler.set_keyboard_action(pygame.K_a, "p1_left")
 InputHandler.set_keyboard_action(pygame.K_d, "p1_right")
 InputHandler.set_keyboard_action(pygame.K_w, "p1_up")
 InputHandler.set_keyboard_action(pygame.K_s, "p1_down")
-InputHandler.set_keyboard_action(pygame.K_SPACE, "p1_main")
-InputHandler.set_keyboard_action(pygame.K_e, "p1_secondary")
+InputHandler.set_keyboard_action(pygame.K_e, "p1_main")
+InputHandler.set_keyboard_action(pygame.K_q, "p1_secondary")
 
-# --- Jugador 2: teclado (flechas + Z dash + X secundaria) ---
+# --- Jugador 2: teclado (flechas + SPACE dash + ENTER/RETURN secundaria) ---
 InputHandler.set_keyboard_action(pygame.K_LEFT, "p2_left")
 InputHandler.set_keyboard_action(pygame.K_RIGHT, "p2_right")
 InputHandler.set_keyboard_action(pygame.K_UP, "p2_up")
 InputHandler.set_keyboard_action(pygame.K_DOWN, "p2_down")
-InputHandler.set_keyboard_action(pygame.K_z, "p2_main")
-InputHandler.set_keyboard_action(pygame.K_x, "p2_secondary")
+InputHandler.set_keyboard_action(pygame.K_SPACE, "p2_main")
+InputHandler.set_keyboard_action(pygame.K_RETURN, "p2_secondary")
 
-# --- Jugador 3: mando (gamepad_id=0) ---
-# Movimiento con el STICK DERECHO
-InputHandler.set_gamepad_axis_action(GAMEPAD_AXIS_RIGHT_X, "axis_x", gamepad_id=0)
-InputHandler.set_gamepad_axis_action(GAMEPAD_AXIS_RIGHT_Y, "axis_y", gamepad_id=0)
-# D-pad también sirve para moverse (alternativa al stick)
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_DPAD_LEFT, "p3_left", gamepad_id=0)
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_DPAD_RIGHT, "p3_right", gamepad_id=0)
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_DPAD_UP, "p3_up", gamepad_id=0)
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_DPAD_DOWN, "p3_down", gamepad_id=0)
-# Dash con el botón X
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_X, "p3_main", gamepad_id=0)
-InputHandler.set_gamepad_button_action(GAMEPAD_BUTTON_B, "p3_secondary", gamepad_id=0)
+# --- Jugador 3: teclado (flechas + P dash + O secundaria) ---
+InputHandler.set_keyboard_action(pygame.K_j, "p3_left")
+InputHandler.set_keyboard_action(pygame.K_l, "p3_right")
+InputHandler.set_keyboard_action(pygame.K_i, "p3_up")
+InputHandler.set_keyboard_action(pygame.K_k, "p3_down")
+InputHandler.set_keyboard_action(pygame.K_p, "p3_main")
+InputHandler.set_keyboard_action(pygame.K_o, "p3_secondary")
 
-# TODO: jugador 4 por un segundo mando (gamepad_id=1), mismo patrón que
-# el jugador 3 pero con gamepad_id=1.
+# --- Jugador 4: teclado (flechas + Y dash + R secundaria) ---
+InputHandler.set_keyboard_action(pygame.K_f, "p4_left")
+InputHandler.set_keyboard_action(pygame.K_h, "p4_right")
+InputHandler.set_keyboard_action(pygame.K_t, "p4_up")
+InputHandler.set_keyboard_action(pygame.K_g, "p4_down")
+InputHandler.set_keyboard_action(pygame.K_y, "p4_main")
+InputHandler.set_keyboard_action(pygame.K_r, "p4_secondary")
 
 # TODO: resolución de ventana / resolución virtual
 WINDOW_WIDTH = 700
